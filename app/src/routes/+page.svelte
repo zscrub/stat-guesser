8f77abfd591fa75f60952ed0c3897e5a99b08b90
Zack <rubinzack40@gmail.com>
2023-05-20 22:55:55 -0400

    tailwind set up

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+
+</script>
+
+<h1 class="text-3xl font-bold underline">Welcome to yup</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
